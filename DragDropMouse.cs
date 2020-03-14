bf4528d92d3ffaedfd547fc64e5c3702711b2145
evgesnia <45089351+evgesnia@users.noreply.github.com>
2020-03-14 13:30:33 +0400

    Add files via upload
    script

---
+++ DragDropMouse.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragDropMouse : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private Vector3 curScreenPoint;
+    private Vector3 curPosition;
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+    void OnMouseDrag()
+    {
+        curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+    void OnMouseUp()
+    {
+
+    }
+}
